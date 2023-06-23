ea2d6f2d137ec2d6911b4ab830a9cb3f45914721
Snehalaxmi-S <snehalaxmi2104@gmail.com>
2023-06-23 16:40:59 +0530

    Updated hero section

--- my-app/src/components/home/hero/Hero.jsx
+++ my-app/src/components/home/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./hero.css";
+import Title from '../../common/title/Title';
 
 const Hero = () => {
   return (
@@ -7,18 +8,20 @@ const Hero = () => {
         <section className="hero">
             <div className="container">
                 <div className="row">
+                    <Title subtitle='WELCOME TO ACADEMIA' title="Best Online Education"/>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia sed officiis, hic laborum similique rem possimus dolor nihil accusantium eligendi a, vitae ratione? Beatae provident distinctio impedit reiciendis, molestiae vero.</p>
                     <div className="button">
                         <button className="primary-btn">
                             GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
                         </button>
-                        <button className="primary-btn">
+                        <button>
                             VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
                         </button>
                     </div>
                 </div>
             </div>
         </section>
+        <div className="margin"></div>
     </>
   )
 }
